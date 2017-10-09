a10ae64d4e6ca3503d721157a83b31aa1715edcb
John Neil Soo <neil.soo@adish.co.jp>
2017-10-09 14:55:23 +0800

    attempt to fix runtimerror in eventlet only in production

--- chat.py
+++ chat.py
@@ -1,4 +1,9 @@
 #!/bin/env python
+import eventlet
+
+eventlet.sleep()
+eventlet.monkey_patch()
+
 from app import create_app, socketio, database
 from flask import g, jsonify, request, session
 import subprocess as sp
